06a7c308e20cc511da7589fbca7a79603d10724c
Guillermo Purro <gpurro@gmail.com>
2024-05-06 09:58:27 -0300

    Update image paths in ExpandingCardsPage.tsx

--- src/features/ExpandingCards/pages/ExpandingCardsPage.tsx
+++ src/features/ExpandingCards/pages/ExpandingCardsPage.tsx
@@ -8,27 +8,27 @@ export const ExpandingCardsPage = () => {
         <Panel
           active
           title="Mountains and Clouds"
-          img="/public/expanding-cards/image1.avif"
+          img="/expanding-cards/image1.avif"
           id="panel-1"
         />
         <Panel
           title="City on Winter"
-          img="/public/expanding-cards/image2.avif"
+          img="/expanding-cards/image2.avif"
           id="panel-2"
         />
         <Panel
           title="Sunny Beach"
-          img="/public/expanding-cards/image3.avif"
+          img="/expanding-cards/image3.avif"
           id="panel-3"
         />
         <Panel
           title="Wild Forest"
-          img="/public/expanding-cards/image4.avif"
+          img="/expanding-cards/image4.avif"
           id="panel-4"
         />
         <Panel
           title="Explore the World"
-          img="/public/expanding-cards/image5.avif"
+          img="/expanding-cards/image5.avif"
           id="panel-5"
         />
       </Panels>
